d7e0e51c17876c4a46f086f8d149522defd126b9
Peter Hille (png!das-system) <peter@das-system-networks.de>
2012-03-13 13:44:50 +0100

    extend tests for ServerInfo's operators

--- tests/serverinfo-operators.cc
+++ tests/serverinfo-operators.cc
@@ -10,9 +10,9 @@ int main(void) {
   ServerInfo a2(a1);
   ServerInfo b("irc.das-system-networks.de", 6667);
 
-  qDebug() << "a1: " << a1.toString();
-  qDebug() << "a2: " << a2.toString();
-  qDebug() << "b : " << b.toString();
+  qDebug() << "a1:" << a1.toString();
+  qDebug() << "a2:" << a2.toString();
+  qDebug() << "b :" << b.toString();
 
   if (a1 != a2) {
     qDebug() << "a1 != a2: inequality operator fail!";
@@ -26,6 +26,19 @@ int main(void) {
     return EXIT_FAILURE;
   } else {
     qDebug() << "a1 != b: success!";
+  }
+
+  qDebug() << "Assigning a2 = b; a2:"
+	   << a2.toString() << "; b:"
+	   << b.toString();
+  a2 = b;
+  qDebug() << "a2 after assignment: " << a2.toString();
+
+  if (a2 != b) {
+    qDebug() << "a2 != b after assignment!";
+    return EXIT_FAILURE;
+  } else {
+    qDebug() << "Assignment success!";
   }
 
   return EXIT_SUCCESS;
