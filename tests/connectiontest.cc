ad48d9dae48615658dac700712fbff6a0a20add0
Peter Hille (png!das-system) <peter@das-system-networks.de>
2012-03-14 17:44:09 +0100

    add simple testing program for QIRC::Connection

---
+++ tests/connectiontest.cc
@@ -0,0 +1,34 @@
+#include <QDebug>
+#include "connectiontest.h"
+
+ConnectionTest::ConnectionTest(QObject* parent) : QObject(parent) {
+}
+
+void ConnectionTest::conn_connected(QIRC::ServerInfo si) {
+  qDebug() << "Connected to" << si;
+}
+
+void ConnectionTest::conn_disconnected(QIRC::ServerInfo si) {
+  qDebug() << "Disconnected from" << si;
+  emit finished();
+}
+
+void ConnectionTest::run() {
+  try {
+    QIRC::ServerInfo si("172.16.1.129", 6667);
+    m_connection = new QIRC::Connection(si);
+  }
+
+  catch (std::bad_alloc& ex) {
+    qCritical() << "Unable to allocate memory for m_connection:"
+		<< ex.what();
+    exit(1);
+  }
+
+  QObject::connect(m_connection, SIGNAL(connected(QIRC::ServerInfo)),
+		   this, SLOT(conn_connected(QIRC::ServerInfo)));
+  QObject::connect(m_connection, SIGNAL(disconnected(QIRC::ServerInfo)),
+		   this, SLOT(conn_disconnected(QIRC::ServerInfo)));
+
+  m_connection->connect();
+}
---
+++ tests/connectiontest.h
@@ -0,0 +1,26 @@
+#ifndef CONNECTIONTEST_H
+#define CONNECTIONTEST_H 1
+
+#include "../Connection"
+#include <QObject>
+
+class ConnectionTest : public QObject {
+  Q_OBJECT
+ public:
+  ConnectionTest(QObject* parent=0);
+
+ public slots:
+  void run();
+
+ protected:
+  QIRC::Connection* m_connection;
+
+  protected slots:
+  void conn_connected(QIRC::ServerInfo si);
+  void conn_disconnected(QIRC::ServerInfo si);
+
+ signals:
+  void finished();
+};
+
+#endif // !CONNECTIONTEST_H
