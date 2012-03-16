b249da8b580fea493fccf987865cdc15080e3748
Peter Hille (png!das-system) <peter@das-system-networks.de>
2012-03-16 02:36:10 +0100

    add FindQIRC.cmake

---
+++ FindQIRC.cmake
@@ -0,0 +1,34 @@
+if(QIRC_INCLUDE_DIR)
+  set(QIRC_FIND_QUIETLY TRUE)
+endif(QIRC_INCLUDE_DIR)
+
+find_path(QIRC_INCLUDE_DIR QIRC/connection.h
+  PATHS
+  "$ENV{QIRC}/include"
+  /usr/local/include
+  /usr/pkg/include
+  /usr/include
+)
+
+find_libraries(QIRC_LIBRARIES QIRC
+  PATHS
+  "$ENV{QIRC}/lib"
+  /usr/local/lib
+  /usr/pkg/lib
+  /usr/lib
+)
+
+set(QIRC_FOUND FALSE)
+if(QIRC_INCLUDE_DIR)
+  if(QIRC_LIBRARIES)
+    set(QIRC_FOUND TRUE)
+    if(NOT QIRC_FIND_QUIETLY)
+      message(STATUS "Found libQIRC: ${QIRC_LIBRARIES}")
+    endif(NOT QIRC_FIND_QUIETLY)
+  endif(QIRC_LIBRARIES)
+endif(QIRC_INCLUDE_DIR)
+
+mark_as_advanced(
+  QIRC_INCLUDE_DIR
+  QIRC_LIBRARIES
+)
