dea0a7d1011a07554fbfa885788181ec2679aebf
Peter Hille (png!das-system) <peter@das-system-networks.de>
2012-04-25 14:51:54 +0200

    add __declspec(dllexport) macro on windows

--- qirc.h
+++ qirc.h
@@ -3,9 +3,19 @@
 
 #include <QString>
 
+#if defined(_WIN32)
+#if defined(QIRC_EXPORTS)
+#define QIRC_EXPORT __declspec(dllexport)
+#else
+#define QIRC_EXPORT __declspec(dllimport)
+#endif // !QIRC_EXPORTS
+#else
+#define QIRC_EXPORT
+#endif // !_WIN32
+
 namespace QIRC {
-  QString stripFormat(QString s);
-  QString stripColors(QString s);
+  QString QIRC_EXPORT stripFormat(QString s);
+  QString QIRC_EXPORT stripColors(QString s);
 };
 
 #endif // !QIRC_H
