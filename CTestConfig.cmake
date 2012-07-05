e747fa2ae4a1305573860a6d5ea66e31d0b4055e
Peter Hille (png!das-system) <peter@das-system-networks.de>
2012-07-05 05:44:06 +0200

    add CTest/CDash configuration

---
+++ CTestConfig.cmake
@@ -0,0 +1,12 @@
+set(CTEST_PROJECT_NAME "libQIRC")
+set(CTEST_NIGHTLY_START_TIME "01:00:00 UTC")
+
+if(NOT DEFINED CTEST_DROP_METHOD)
+  set(CTEST_DROP_METHOD "http")
+endif(NOT DEFINED CTEST_DROP_METHOD)
+
+if(CTEST_DROP_METHOD STREQUAL "http")
+  set(CTEST_DROP_SITE "harpy.png.das-system.vpn")
+  set(CTEST_DROP_LOCATION "/cdash/submit.php?project=libQIRC")
+  set(CTEST_TRIGGER_SITE "http://${CTEST_DROP_SITE}/")
+endif(CTEST_DROP_METHOD STREQUAL "http")
